4b9991dbbc391c4180e790704811cbba3cff2f6c
Michael Goodness <mike@opsgoodness.net>
2023-02-15 18:04:37 -0600

    chore(fish): update man colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,7 +11,7 @@ set -gx GIT_WORKSPACE ~/Code
 set -gx LESS --incsearch --ignore-case --jump-target=.5 --LONG-PROMPT --raw-control-chars --quit-if-one-screen
 set -gx LESSCHARSET utf-8
 set -gx man_bold --bold $fish_color_command
-set -gx man_standout --reverse F0CB02
+set -gx man_standout --reverse $fish_color_search_match
 set -gx man_underline --underline $fish_color_param
 set -gx PAGER less
 set -gx USE_GKE_GCLOUD_AUTH_PLUGIN True
