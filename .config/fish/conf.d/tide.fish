d305e42d34cc539c411b652d83cbad9f3b2e1fe1
Michael Goodness <mike@opsgoodness.net>
2023-02-16 12:18:17 -0600

    feat(fish): update prompt

--- .config/fish/conf.d/tide.fish
+++ .config/fish/conf.d/tide.fish
@@ -7,17 +7,18 @@ status is-interactive && status is-login || exit
 
 set -gx fish_greeting
 set -g fish_handle_reflow 0 # https://github.com/fish-shell/fish-shell/issues/1706#issuecomment-803655785
-set -gx tide_direnv_color yellow
 set -gx tide_direnv_bg_color normal
+set -gx tide_direnv_color yellow
 set -gx tide_direnv_denied_color brred
-set -gx tide_gcloud_color blue
 set -gx tide_gcloud_bg_color normal
-set -gx tide_kubectl_icon ﴱ
+set -gx tide_gcloud_color blue
+set -gx tide_gcloud_icon 
+set -gx tide_kubectl_icon 󱃾
+# set -gx tide_node_color 68A063
 set -gx tide_node_icon 
-set -gx tide_node_color 68A063
 set -gx tide_rustc_icon R
-set -gx tide_tea_color cyan
 set -gx tide_tea_bg_color normal
+set -gx tide_tea_color cyan
 
 string match -q "$TERM_PROGRAM" vscode && set -gx tide_shlvl_threshold "$SHLVL"
 
