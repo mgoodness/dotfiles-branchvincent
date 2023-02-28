ad33fe3a72835f01513a59682005150001e1a90e
Michael Goodness <mike@opsgoodness.net>
2023-02-27 23:10:30 -0600

    chore(yadm): rename bootstrap scripts

--- .config/yadm/bootstrap.d/02-macos.sh##os.Darwin,e.sh
+++ .config/yadm/bootstrap.d/02-macos.sh##os.Darwin,e.sh
@@ -16,20 +16,20 @@ fi
 
 ### Dock ###
 debug "Dock"
-defaults write com.apple.dock orientation -string bottom     # Place at bottom
-defaults write com.apple.dock show-recents -bool false       # Hide recent apps
-defaults write com.apple.dock minimize-to-application -int 1 # Minimize apps into itself
-defaults write com.apple.dock magnification -int 1           # Enable magnification
+defaults write com.apple.dock orientation -string bottom # Place at bottom
+defaults write com.apple.dock show-recents -bool false   # Hide recent apps
+defaults write com.apple.dock magnification -int 1       # Enable magnification
 defaults write com.apple.dock largesize -int 80
 dockutil --no-restart --remove all
 dockutil --no-restart --add /System/Applications/Calendar.app
 dockutil --no-restart --add /System/Applications/Mail.app
 dockutil --no-restart --add /System/Applications/Messages.app
 dockutil --no-restart --add /Applications/Slack.app
-dockutil --no-restart --add /Applications/Spotify.app
-dockutil --no-restart --add /Applications/Google\ Chrome.app
-dockutil --no-restart --add /Applications/Visual\ Studio\ Code.app
+dockutil --no-restart --add /System/Applications/Music.app
+dockutil --no-restart --add /System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app # https://github.com/kcrawford/dockutil/issues/144
 dockutil --no-restart --add /Applications/iTerm.app
+dockutil --no-restart --add /System/Applications/Notes.app
+dockutil --no-restart --add /System/Applications/Reminders.app
 dockutil --no-restart --add /System/Applications/System\ Settings.app
 dockutil --no-restart --add ~/Documents --sort name --display folder --view list
 dockutil --no-restart --add ~/Downloads --sort dateadded --display folder --view fan
@@ -47,14 +47,6 @@ for ext in public.{data,json,plain-text,python-script,shell-script,source-code,t
     duti -s com.microsoft.VSCode "$ext" all # Set VSCode as default app for code
 done
 
-### iTerm2 ###
-debug "iTerm"
-# shellcheck disable=SC2088
-defaults write com.googlecode.iterm2.plist DynamicProfilesPath -string "~/.config/iterm2"
-defaults write com.googlecode.iterm2.plist "Default Bookmark Guid" -string 382C82BD-43A8-4CF0-95F4-B75123F1F8DC
-defaults write com.googlecode.iterm2.plist OptionIsMetaForSpecialChars -int 0
-defaults write com.googlecode.iterm2.plist PromptOnQuit -int 0
-
 ### Mission Control ###
 debug "Mission Control"
 defaults write com.apple.dock mru-spaces -bool false
@@ -63,9 +55,9 @@ defaults write com.apple.dock wvous-tr-corner -int 12 # Top right: Notification 
 
 ### Sharing ###
 debug "Sharing"
-if [ "$(scutil --get ComputerName)" != "Branch's MacBook" ]; then
-    scutil --set ComputerName "Branch's MacBook"
-    scutil --set LocalHostName "Branchs-MacBook"
+if [ "$(scutil --get ComputerName)" != "Goodness's MacBook Pro" ]; then
+    scutil --set ComputerName "Goodness's MacBook Pro"
+    scutil --set LocalHostName "Goodness-MacBook-Pro"
 fi
 
 ### Siri ###
@@ -82,7 +74,7 @@ defaults write -g com.apple.trackpad.scaling 3 # Max trackpad speed
 
 ### Wallpaper ###
 debug "Wallpaper"
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/System/Library/Desktop Pictures/Monterey Graphic.heic"'
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/System/Library/Desktop Pictures/Ventura Graphic.heic"'
 
 # Restart affected apps
 debug "Restarting apps"
